455a87de536ec60f5f6ff21ec727d9015adc9ef7
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-02 01:31:09 +0300

    add Task7 print third number

---
+++ Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача №7. Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+//456 -> 6; 782 -> 2; 918 -> 8
+Console.WriteLine("Введите трехзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2)
+{
+  Console.WriteLine("Третья цифра: " + NumberText[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет");
+}
