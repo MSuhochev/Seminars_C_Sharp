82b0419940a0a8a3dc78b20f0491728e60064c57
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-01 17:54:32 +0300

    add Task4 second homework task

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход
+//три числа и выдает максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b & a > c)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+if(b > c & b > a)
+{
+    Console.Write("max = ");
+    Console.Write(b); 
+}
+if(c > b & c > a)
+{
+        Console.Write("max = ");
+        Console.Write(c);
+}
