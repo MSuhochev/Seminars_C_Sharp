4d415c1b6cedf9b0e46ecef40d2405ccfb3ed581
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-03 21:25:11 +0300

    add Task11 random number xxx deleting second num and out first and third numbers

---
+++ Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру 
+// этого числа.
+// 456 -> 46, 782 -> 72, 918 -> 98
+
+int num = new Random().Next(100, 1000);
+int digit2 = num % 10;
+int digit1 = num / 100;
+Console.WriteLine($"Случайное число = {num}, Первая цифра = {digit1}, Третья цифра = {digit2}");
+int summ = digit1 * 10 + digit2;
+Console.WriteLine(summ);
