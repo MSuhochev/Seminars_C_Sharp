278bdc09bab5b067e85f5cf5d99c38349364ab83
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-03 21:30:54 +0300

    add Task9 max from random number

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+// Например: 78 -> 8, 12-> 2, 85 -> 8
+
+int num = new Random().Next(10, 100);
+int digit2 = num % 10;
+int digit1 = num / 10;
+//Console.WriteLine("Случайное число = " + num + "Первая цифра = " + digit1 +)
+Console.WriteLine($"Случайное число = {num}, Первая цифра = {digit1}, Вторая цифра = {digit2}");
+int max = digit1;
+if(max<digit2)
+{
+    max = digit2;
+}
+Console.WriteLine(max);
