9b9135584c3ec723e20821dc28fa977269badc4a
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-04 21:10:27 +0300

    add cycle to solution Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -8,26 +8,9 @@ if(num < 100)
 {
     Console.WriteLine("Третьей цифры нет!");
 }
-else if(num > 99 && num < 1000)
+while(num > 1000)
 {
-    num = num % 10;
-    Console.WriteLine(num);
-}
-else if(num > 999 && num < 10000)
-{
-    num = num % 100;
     num = num / 10;
-    Console.WriteLine(num);
 }
-else if(num > 10000 && num < 100000)
-{
-    num = num % 1000;
-    num = num / 100;
-    Console.WriteLine(num);
-}
-else if(num > 100000 && num < 1000000)
-{
-    num = num % 10000;
-    num = num / 1000;
-    Console.WriteLine(num);
-}
+num = num % 10;
+Console.WriteLine(num);
