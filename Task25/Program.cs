89741aae03a2d30e85e314dee94f9b6e80df4ae7
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-13 19:40:50 +0300

    add Task25 HomeWork raising number a to power b

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 25: 
+Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵); 2, 4 -> 16 */
+
+Console.WriteLine("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В - (оно будет являться степенью \nв которую необходимо возвести число А):");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = a;
+int count = 1;
+while(count < b)
+{
+    res = a * res;
+    count++; 
+}
+Console.WriteLine($"Число {a} в {b} степени = {res}");
