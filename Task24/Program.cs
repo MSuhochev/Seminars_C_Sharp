90f3b1d0fe7f754727bf7b6128043be977bc0be0
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-13 19:37:46 +0300

    add Task24 function return sum from 1 to A

---
+++ Task24/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//считывает целое число от пользователя
+int ReadInt(string message)
+{
+   Console.WriteLine(message);
+   return Convert.ToInt32(Console.ReadLine());
+
+}
+int GetSumToNumber(int number)  //возвращает сумму от 1 до намбер
+{
+    int sum = 0;
+    for(int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+
+}
+int A = ReadInt("Введите число: ");
+int result = GetSumToNumber(A);
+Console.WriteLine($"Сумма чисел от 1 до {A} = {result}");
+
