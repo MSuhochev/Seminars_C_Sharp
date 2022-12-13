b54fdcf99df6e4d60c96370c6a54cd1a7d3f3f6a
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-13 19:44:19 +0300

    add Task27 sum of digits in a number

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 27: 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11;  82 -> 10; 9012 -> 12      */
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sumnum = 0;
+int tmpnum = num;
+while(tmpnum > 0)
+{
+    sumnum += tmpnum % 10;
+    tmpnum /= 10;
+}
+Console.WriteLine($"Сумма цифр числа {num} равна {sumnum}");
