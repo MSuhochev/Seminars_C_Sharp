e099f9c29d2db5e6991db5efaa5cc7a992352076
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-22 21:11:20 +0300

    add Task42 create PrintBinaryView

---
+++ Task42/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 42: Напишите программу, которая будет 
+преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+Console.WriteLine("Введите десятичное число для преобразования этого числа в двоичное:");
+int num = Convert.ToInt32(Console.ReadLine());
+PrintBinaryView(num);
+void PrintBinaryView(int n)
+{
+    if(n <= 0) return;
+    PrintBinaryView(n / 2);
+    Console.Write(n % 2);
+}
