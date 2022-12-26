c6321e988ce64d4e98a9939671a319b6a400f7d8
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-27 00:42:56 +0300

    add Task62 create 2D Spiral Array

---
+++ Task62/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write(String.Format("{0,3}", array[i, j]));
+
+      else Console.Write(String.Format("{0,3}", array[i, j]));
+    }
+    Console.WriteLine();
+  }
+}
+
+int rows = 4,
+columns = 4;
+int[,] spiralMatrix = new int[rows, columns];
+int tmp = 1,
+i = 0,
+j = 0;
+
+while (tmp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
+{
+  spiralMatrix[i, j] = tmp;
+  tmp++;
+  if (i <= j + 1 && i + j < spiralMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= spiralMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > spiralMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(spiralMatrix);
