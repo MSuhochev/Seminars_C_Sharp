509fc724efca13f2ca8cf99e3f27ecb7fcadca44
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-26 23:44:24 +0300

    add Task45 create copy array

---
+++ Task45/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.*/
+
+int[] array = {1,2,3,4,5};
+int[] copyarray = new int[array.Length];
+for (int i = 0; i < copyarray.Length; i++)
+{
+    copyarray[i] = array[i];
+}
+Console.WriteLine(string.Join(", ", copyarray));
+
+
+
