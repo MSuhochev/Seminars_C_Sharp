6aee7ae4ee09ad138dc8e4860bba1e0cf8a1ee72
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-27 00:18:15 +0300

    add Task52 create 2D Arr integers. Find the arithmetic mean of the elements in each column.

---
+++ Task52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// ​Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4   Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rows, columns];
+
+    var rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matr;
+}
+
+void SredArefColumnsMatrix(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            int srAr = 0;
+            for (i = 0; i < mas.GetLength(0); i++)
+            {
+                srAr += mas[i, j];
+            }
+            Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {(float)srAr / mas.GetLength(0):f1}");
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,3}", matr[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+
+int rowsCount = ReadInt("Введите число строк");
+int columnsCount = ReadInt("Введите число столбцов");
+Console.WriteLine();
+int[,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
+Console.WriteLine();
+SredArefColumnsMatrix(matrix);
+
