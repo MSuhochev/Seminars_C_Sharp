e591f719b0d9cc202c9596fc3032d0b57d6f0ae9
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-28 18:16:02 +0300

    add Task67 SUMM digit of  number using recursion

---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine())); //math.abs убираем минус(если ввели отрицательное число)
+int Sumnum(int sum)
+{
+    if (sum==0) return 0;
+    else
+    {
+        sum = sum%10 + Sumnum(sum / 10);
+    }
+    return sum;
+}
+
+Console.WriteLine(Sumnum(num));
