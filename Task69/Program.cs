8005183cbc3e9dfae4b7ebd00f576b83f8b2ddec
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-28 18:18:40 +0300

    add Task69 raises the number A to the integer power of B using recursion

---
+++ Task69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число A");
+int num = Convert.ToInt32(Console.ReadLine()); //math.abs убираем минус(если ввели отрицательное число)
+Console.WriteLine("Введите число B");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int Product(int n, int m)
+{
+    if (m==0) return 1;
+    else
+    {
+        n *= Product(n, m -1);
+    }
+    return n;
+}
+
+Console.WriteLine(Product(num, num2));
