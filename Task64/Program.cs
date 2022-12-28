731bcc3f00c07b56d053ba9775b7b4e4ee86f73f
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-28 18:08:45 +0300

    add Task64 print all natural numbers from N to 1 using recursion

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int LineRevers(int min, int max)
+{
+    if (max == min) return min;
+    if (max < min) 
+    Console.Write(LineRevers(min - 1, max) + ", ");
+    else 
+    Console.Write(LineRevers(min + 1, max) + ", ");
+    return min;
+}
+
+int min = 1;
+int max = ReadInt("Введите число:");
+Console.WriteLine();
+Console.WriteLine(LineRevers(min, max));
+Console.WriteLine();
